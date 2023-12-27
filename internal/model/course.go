fe2194a79691754f268a595018d77b59ad508a98
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-12-27 08:34:23 +0300

    модель для курса

---
+++ internal/model/course.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+	"time"
+)
+
+type Course struct {
+	ID          int       `db:"id" json:"id"`
+	CreatedBy   int       `db:"created_by" json:"created_by"`
+	IsActive    bool      `db:"active" json:"active"`
+	IsArchived  bool      `db:"archived" json:"archived"`
+	Name        string    `db:"name" json:"name"`
+	Description string    `db:"description" json:"description"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
+}
