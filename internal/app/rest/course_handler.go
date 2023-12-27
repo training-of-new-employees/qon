07d863325c9d251ad77a31ccf145b91e423d0b84
Nexadis <37233902+Nexadis@users.noreply.github.com>
2023-12-27 08:35:25 +0300

    получение курсов

--- internal/app/rest/course_handler.go
+++ internal/app/rest/course_handler.go
@@ -1,10 +1,27 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	"github.com/training-of-new-employees/qon/internal/model"
 )
 
 func (r *RestServer) handlerGetCourses(c *gin.Context) {
+	ctx := c.Request.Context()
+	us := r.getUserSession(c)
+	u := model.User{
+		ID:        us.UserID,
+		IsAdmin:   us.IsAdmin,
+		CompanyID: us.OrgID,
+	}
+	courses, err := r.services.Course().GetCourses(ctx, u)
+	if err != nil {
+		r.handleError(c, err)
+		return
+	}
+	c.JSON(http.StatusOK, courses)
 
 }
 func (r *RestServer) handlerCreateCourse(c *gin.Context) {
