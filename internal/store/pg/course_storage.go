25bff75e788cf90dfb20869f9d04124e9debd7c0
Nexadis <37233902+Nexadis@users.noreply.github.com>
2024-01-15 15:21:33 +0300

    Тесты для store-слоя

--- internal/store/pg/course_storage.go
+++ internal/store/pg/course_storage.go
@@ -2,6 +2,7 @@ package pg
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/jmoiron/sqlx"
 
@@ -31,8 +32,14 @@ func (c *courseStorage) UserCourses(ctx context.Context, userID int) ([]model.Co
 	JOIN courses c ON pc.course_id = c.id
 	WHERE u.id = $1`
 	err := c.tx(func(tx *sqlx.Tx) error {
-		return tx.SelectContext(ctx, courses, qCourses, userID)
+		return tx.SelectContext(ctx, &courses, qCourses, &userID)
 	})
+	if err != nil {
+		return nil, handleError(err)
+	}
+	if len(courses) == 0 {
+		return nil, handleError(sql.ErrNoRows)
+	}
 	return courses, handleError(err)
 
 }
@@ -43,8 +50,14 @@ func (c *courseStorage) CompanyCourses(ctx context.Context, companyID int) ([]mo
 	JOIN users u ON u.id = c.created_by
 	WHERE u.company_id = $1`
 	err := c.tx(func(tx *sqlx.Tx) error {
-		return tx.SelectContext(ctx, courses, qCourses, companyID)
+		return tx.SelectContext(ctx, &courses, qCourses, &companyID)
 	})
+	if err != nil {
+		return nil, handleError(err)
+	}
+	if len(courses) == 0 {
+		return nil, handleError(sql.ErrNoRows)
+	}
 	return courses, handleError(err)
 }
 
@@ -61,13 +74,21 @@ func (c *courseStorage) CreateCourse(ctx context.Context, course model.CourseSet
 
 func (c *courseStorage) EditCourse(ctx context.Context, course model.CourseSet, companyID int) (*model.Course, error) {
 	var res model.Course
-	qEdit := `UPDATE courses c
-	JOIN users u ON c.created_by = u.id
-	SET c.name=COALESCE($1,c.name), c.description=COALESCE($2,c.description), c.archived=$3
-	WHERE c.id=$4 AND u.company_id=$5
+	qEdit := `UPDATE courses AS c
+	SET name=COALESCE($1,c.name), description=COALESCE($2,c.description), archived=$3
+	FROM users u  
+	WHERE c.id=$4 AND u.company_id=$5 AND c.created_by = u.id
 	RETURNING c.id, c.created_by, c.active, c.archived, c.name, c.description, c.created_at, c.updated_at`
+	n := &course.Name
+	d := &course.Description
+	if course.Name == "" {
+		n = nil
+	}
+	if course.Description == "" {
+		d = nil
+	}
 	err := c.tx(func(tx *sqlx.Tx) error {
-		return tx.GetContext(ctx, &res, qEdit, course.Name, course.Description, course.IsArchived, course.ID, companyID)
+		return tx.GetContext(ctx, &res, qEdit, n, d, &course.IsArchived, &course.ID, &companyID)
 	})
 	return &res, handleError(err)
 
