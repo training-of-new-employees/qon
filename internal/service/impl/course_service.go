ab3bb20b94384bdb7156ff1e87ac975604e0e060
Nexadis <37233902+Nexadis@users.noreply.github.com>
2024-01-21 17:22:43 +0300

    Получение курсов с проверкой их отсутствия

--- internal/service/impl/course_service.go
+++ internal/service/impl/course_service.go
@@ -23,11 +23,24 @@ func newCourseService(db store.Storages) *courseService {
 }
 
 func (cs *courseService) GetUserCourses(ctx context.Context, userID int) ([]model.Course, error) {
-	return cs.db.CourseStorage().UserCourses(ctx, userID)
-
+	crs, err := cs.db.CourseStorage().UserCourses(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	if len(crs) == 0 {
+		return nil, errs.ErrCourseNotFound
+	}
+	return crs, nil
 }
 func (cs *courseService) GetCompanyCourses(ctx context.Context, companyID int) ([]model.Course, error) {
-	return cs.db.CourseStorage().CompanyCourses(ctx, companyID)
+	crs, err := cs.db.CourseStorage().CompanyCourses(ctx, companyID)
+	if err != nil {
+		return nil, err
+	}
+	if len(crs) == 0 {
+		return nil, errs.ErrCourseNotFound
+	}
+	return crs, nil
 }
 
 func (cs *courseService) CreateCourse(ctx context.Context, c model.CourseSet) (*model.Course, error) {
